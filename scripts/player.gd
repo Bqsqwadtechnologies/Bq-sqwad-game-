81bab260d96c0dbb1cd00cbbc187bd1d142cc8f8
bqsqwadai <isaacoshiomole0@gmail.com>
2026-09-15 16:39:04 +0100

    Fix and complete Ziking prototype controller

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,12 +4,13 @@ extends CharacterBody3D
 @export var acceleration := 22.0
 @export var gravity := 20.0
 @export var jump_velocity := 7.0
+@export var turn_speed := 10.0
 
 func _physics_process(delta: float) -> void:
     var input_vector := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
     var direction := Vector3(input_vector.x, 0.0, input_vector.y)
 
-    if direction.length() > 1.0:
+    if direction.length_squared() > 1.0:
         direction = direction.normalized()
 
     var target := direction * move_speed
@@ -20,5 +21,11 @@ func _physics_process(delta: float) -> void:
         velocity.y -= gravity * delta
     elif Input.is_action_just_pressed("jump"):
         velocity.y = jump_velocity
+    else:
+        velocity.y = 0.0
+
+    if direction.length_squared() > 0.01:
+        var target_rotation := atan2(direction.x, direction.z)
+        rotation.y = lerp_angle(rotation.y, target_rotation, turn_speed * delta)
 
     move_and_slide()
